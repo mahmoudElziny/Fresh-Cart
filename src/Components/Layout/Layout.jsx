1117f68a615e893f8d1a08dcc24d81cd6b772faa
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2024-10-29 19:31:30 +0300

    detect offline

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
+import { Offline } from 'react-detect-offline'
 
 export default function Layout() {
     
   return (
     <>
+      <div className="network alert">
+        <Offline>You're offline right now. Check your connection.</Offline>
+      </div>
       <Navbar />
       <div className="container">
         <Outlet />
